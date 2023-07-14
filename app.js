89cf154fd8d6d2b7def794b80cd401a6ad603262
marrjan <marjankhormaeipour@gmail.com>
2023-07-15 03:24:55 +0430

    pull 7

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // start javaScript code
 let userInput = document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
 let answer = document.getElementById("answer");
 
 function calculatAge(){
@@ -11,19 +12,19 @@ function calculatAge(){
 
   let today = new Date();
 
-  let d2 = birthDate.getDate();
-  let m2 = birthDate.getMonth() + 1;
-  let y2 = birthDate.getFullYear();
+  let d2 = today.getDate();
+  let m2 = today.getMonth() + 1;
+  let y2 = today.getFullYear();
 
   let d3 , m3 , y3 ;
   y3 = y2 - y1;
 
   if (m2 >= m1){
-    m3 = m2 - m1
+    m3 = m2 - m1;
   }
   else{
     y3--;
-    m3 = 12 + m2 - m1
+    m3 = 12 + m2 - m1;
   }
 
   if (d2 >= d1){
@@ -31,7 +32,7 @@ function calculatAge(){
   }
   else{
     m3--;
-    d3 = getDaysInMonth(y1,m1) + d2 - d1
+    d3 = getDaysInMonth(y1,m1) + d2 - d1;
   }
   if(m3 < 0){
     m3 = 11;
